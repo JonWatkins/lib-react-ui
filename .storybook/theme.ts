7186cff97b472fe4dea56e33a55a0685288e6cd6
Jon <jonwatkins86@gmail.com>
2023-06-30 20:28:07 +0100

    updating storybook

---
+++ .storybook/theme.ts
@@ -0,0 +1,8 @@
+import { create } from '@storybook/theming/create';
+
+export default create({
+  base: 'dark',
+  brandTitle: 'Lib React UI',
+  brandUrl: 'https://github.com/JonWatkins/lib-react-ui',
+  brandTarget: '_blank',
+});
