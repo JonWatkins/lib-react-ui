e94674cbf1a2033bdcd44618ecc26373595f403e
Jon <jonwatkins86@gmail.com>
2023-06-30 15:53:24 +0100

    Added more button stories

--- src/components/Button/Button.stories.tsx
+++ src/components/Button/Button.stories.tsx
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 import { Meta, StoryObj } from "@storybook/react";
 import { Button } from "./Button";
+import { ThemeColors } from "../../globals";
 
 type Story = StoryObj<typeof Button>;
 
@@ -50,5 +51,54 @@ Large.args = {
   text: "Hello World",
   size: "lg",
 };
+
+export const ButtonWithHooks: Story = () => {
+  const [value, setValue] = useState('secondary')
+  const [isPrimary, setIsPrimary] = useState(false)
+
+  const onClickHandler = () => {
+    if (!isPrimary) {
+      setIsPrimary(true)
+      setValue('primary')
+    } else {
+      setIsPrimary(false)
+      setValue('secondary')
+    }
+  }
+
+  return (
+    <Button color={value as ThemeColors} onClick={onClickHandler} text="Click Me" />
+  )
+}
+
+ButtonWithHooks.args = {
+}
+
+export const AvailableColors: Story = () => {
+  const colors: ThemeColors[] = [
+    "primary",
+    "secondary",
+    "success",
+    "danger",
+    "warning",
+    "info",
+    "lightest",
+    "light",
+    "dark",
+    "muted",
+    "white",
+  ]
+
+  return (
+    <>
+      {colors.map((color) => (
+        <Button color={color} text={color} className="mr-4 mb-4" />
+      ))}
+    </>
+  )
+}
+
+AvailableColors.args = {
+}
 
 export default meta;
