c613cf939aa7c5221b87ad2ca0acf8c955c7e1bb
Jon Watkins <jonwatkins86@gmail.com>
2023-07-14 22:35:55 +0100

    Added new bundles

---
+++ tsup.config.js
@@ -0,0 +1,14 @@
+import { sassPlugin } from "esbuild-sass-plugin";
+import { defineConfig } from "tsup";
+
+export default defineConfig({
+  entry: ["src/index.js"],
+  splitting: true,
+  sourcemap: true,
+  clean: true,
+  dts: true,
+  format: ["esm", "cjs"],
+  target: "es2020",
+  bundle: true,
+  esbuildPlugins: [sassPlugin()],
+});
