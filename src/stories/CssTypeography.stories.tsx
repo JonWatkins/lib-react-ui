42aa2ea2b38806fdad8c6641933ac2485204a844
Jon Watkins <jonwatkins86@gmail.com>
2023-07-14 22:45:11 +0100

    Added css story example files back in

---
+++ src/stories/CssTypeography.stories.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { Meta, StoryObj } from "@storybook/react";
+
+type Story = StoryObj
+
+const meta: Meta<Story> = {
+  title: "CSS Typeography",
+  argTypes: {},
+};
+
+export const Primary: Story = (args) => (
+  <div>
+
+  </div>
+);
+
+Primary.args = {
+
+};
+
+export default meta;
