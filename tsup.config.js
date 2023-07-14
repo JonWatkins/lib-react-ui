07aaa4462080ad0107ee65f6d2c3cc6b8c716181
Jon Watkins <jonwatkins86@gmail.com>
2023-07-14 23:08:34 +0100

    Trying to get the css minifier to work

--- tsup.config.js
+++ tsup.config.js
@@ -1,5 +1,9 @@
 import { sassPlugin } from "esbuild-sass-plugin";
 import { defineConfig } from "tsup";
+import postcss from "postcss";
+import autoprefixer from "autoprefixer";
+import postcssPresetEnv from "postcss-preset-env";
+import cssNano from "cssnano";
 
 export default defineConfig({
   entry: ["src/index.js"],
@@ -10,5 +14,16 @@ export default defineConfig({
   format: ["esm", "cjs"],
   target: "es2020",
   bundle: true,
-  esbuildPlugins: [sassPlugin()],
+  esbuildPlugins: [
+    sassPlugin({
+      async transform(source) {
+        const { css } = await postcss([
+          autoprefixer,
+          postcssPresetEnv({ stage: 0 }),
+          cssNano({ preset: 'default' }),
+        ]).process(source, { from: undefined });
+        return css;
+      },
+    }),
+  ],
 });
