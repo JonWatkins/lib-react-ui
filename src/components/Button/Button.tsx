8d8185ef0b140b5900563a3d345fa1a83fcd0945
Jon Watkins <jonwatkins86@gmail.com>
2023-07-19 22:54:46 +0100

    Updated button padding/bg/font size

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -2,6 +2,12 @@ import React, { FC } from "react";
 import classNames from "classnames";
 import type { ButtonProps } from "./Button.types";
 
+const padding = (size: string): string => {
+  if (['xl', 'large'].indexOf(size) !== -1) return 'pt-3 pr-3 pl-3 pb-3'
+  if (size === 'medium') return 'pt-2 pr-2 pl-2 pb-2'
+  return 'pt-1 pr-1 pl-1 pb-1'
+}
+
 export const Button: FC<ButtonProps> = ({
   size = "small",
   color = "primary",
@@ -12,7 +18,7 @@ export const Button: FC<ButtonProps> = ({
   onClick,
   ...props
 }) => {
-  const classes = [`text-${size}`, `bg-${color}`, className];
+  const classes = [`text-${size}`, `bg-${color}`, padding(size), className];
   return (
     <button
       className={classNames(classes)}
