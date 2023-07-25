9c72f8e0d0257b5a358431cc3c1ac4f3ea624842
Jon Watkins <jonwatkins86@gmail.com>
2023-07-25 21:01:01 +0100

    nicer button styling

--- src/components/Alert/Alert.spec.tsx
+++ src/components/Alert/Alert.spec.tsx
@@ -11,7 +11,8 @@ describe("Alert", () => {
   it("Should be able to render an alert", () => {
     const dom = render(<Alert id="test-alert" />);
     const alert = getById(dom.container, "test-alert");
-    expect(alert).toHaveClass("bg-info/75");
+    expect(alert).toHaveClass("alert");
+    expect(alert).toHaveClass("alert-info");
   });
 
   it("should be able to render content", () => {
@@ -24,7 +25,7 @@ describe("Alert", () => {
     it(`should be able to set the type of the alert to ${color}`, () => {
       const dom = render(<Alert id="test-alert" type={color as ThemeColors} />);
       const alert = getById(dom.container, "test-alert");
-      expect(alert).toHaveClass(`bg-${color}/75`);
+      expect(alert).toHaveClass(`alert-${color}`);
     });
   });
 });
